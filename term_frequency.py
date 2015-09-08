e8e4afc3bb4882cae9ac837c404d161317ca66cb
pramod-kumar-para <pramodkumarpara95@gmail.com>
2015-09-08 19:26:54 +0530

    Update term_frequency.py

--- term_frequency.py
+++ term_frequency.py
@@ -23,8 +23,8 @@ def main(tweetfile):
 
     
 if __name__ == '__main__':
-if len(sys.argv) == 2:
-  main(sys.argv[1])
-else:
-   print('Usage: give the filename containing your tweets as argument')
+  if len(sys.argv) == 2:
+    main(sys.argv[1])
+  else:
+    print('Usage: give the filename containing your tweets as argument')
  
