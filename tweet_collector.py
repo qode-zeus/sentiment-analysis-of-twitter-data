481f2e3a11068d62698ad28c210c0e37c9ffa50b
pramod-kumar-para <pramodkumarpara95@gmail.com>
2015-09-08 19:22:14 +0530

    Update tweet_collector.py

--- tweet_collector.py
+++ tweet_collector.py
@@ -1,10 +1,11 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
-ckey='zdKeglVccs3FPAaJIynm5AC0N'
-csecret='e5zHsXTC245e5vKRXrLxCrB8BUIpwCRNq3lFiBvrYieKHnI4YK'
-atoken='2921267035-M1wKR8bdOCqqgE5XYVnDDnxDHKdkkjaJvOEHg4A'
-asecret='l3UIq1NAx3TVLuzBNRADKvtu3HBo9hWkqfYRIqt2gU6xd'
+# Obtain credentials from twitter
+ckey='enter your consumer key'
+csecret='enter your consumer secret'
+atoken='enter your access token'
+asecret='enter yout access secret'
 class listener(StreamListener):
         def on_data(self,data):
                 try:
